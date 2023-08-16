aadfcd6d5b66e73550af0f50303c53f68994c5da
HYOJIN <phila630@gmail.com>
2023-08-16 21:41:26 +0900

    update dfs stack

--- study/dfs.js
+++ study/dfs.js
@@ -17,19 +17,19 @@ function dfsStack(graph) {
   const visited = Array(graph.length).fill(false);
   const stack = [];
   stack.push(1);
-  visited[1] = true;
-  console.log(1);
 
-  while (stack.length > 0) {
+  while (stack.length) {
     const vertex = stack.pop();
+    console.log(vertex);
 
-    console.log(stack);
+    visited[vertex] = true;
     for (let i = 0; i < graph[vertex].length; i++) {
       const next = graph[vertex][i];
+
       if (!visited[next]) {
         stack.push(next);
-        visited[next] = true;
-        console.log(next);
+
+        break;
       }
     }
   }
@@ -38,5 +38,7 @@ function dfsStack(graph) {
 const graph = [[], [2, 3, 8], [1, 7], [1, 4, 5], [3, 5], [3, 4], [7], [2, 6, 8], [1, 7]];
 const visited = Array(9).fill(false);
 
-// dfs(graph, 1, visited);
+dfs(graph, 1, visited);
+
+console.log('################');
 dfsStack(graph);
